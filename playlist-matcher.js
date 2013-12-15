0d8d35bfd1ebf502fa605c9d13077753e8b6163a
filipesilva <filipematossilva@gmail.com>
2013-12-15 13:43:08 +0000

    filtered out comments in playlist

--- playlist-matcher.js
+++ playlist-matcher.js
@@ -1,3 +1,30 @@
 var path = require('path');
 var fs = require('fs');
 
+var makeHash = function(array){
+    var hash = {};
+    for (var i = array.length - 1; i >= 0; i--) {
+        hash[array[i]] = true;
+    }
+    return hash;
+};
+
+fs.readdir("./", function (err, files) {
+    if(err) {
+        console.log(err);
+    } else {
+        dirHash = makeHash(files);
+        console.log(dirHash);
+    }
+});
+
+fs.readFile("./playlist.small.m3u", 'utf8',function (err, data) {
+    if(err) {
+        console.log(err);
+    } else {
+        var lines = data.toString().split("\r\n")
+                    .filter(function(line){ return line.charAt(0) != '#';});
+        filesHash = makeHash(lines);
+        console.log(filesHash);
+    }
+});
