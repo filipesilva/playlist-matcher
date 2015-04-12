775fd0df54915de24576066c0588743043611b08
filipesilva <filipematossilva@gmail.com>
2015-04-12 13:57:06 +0100

    added subdirs

--- README.md
+++ README.md
@@ -1,31 +1,48 @@
-# Playlist matcher
+Playlist matcher
+================
 
-Playlist matcher is a node script for matching existing files/folders between a playlist and a directory. 
-I use it to filter my music collection for unused and missing files/folders. It can also delete unused files.
+Playlist matcher is a node script for matching existing files/folders
+between a m3u8 playlist and a directory. I use it to filter my music
+collection for unused and missing files/folders, and it will only
+consider folders with mp3/wav/flac/aac files in it.
 
-# Usage
+It can also delete unused files/folders, but this should be done with
+care. Run it without the option first to double check if you really
+want to delete all the directories.
 
-node playlist-matcher.js playlist [del]
+Usage
+=====
 
-## Prerequisites
-* Node.js - Download and Install [Node.js](http://www.nodejs.org/download/).
+Run the following command to produce two json files, unused.json (not in
+playlist) and missing.json (not in file structure). Add in the `del`
+option to remove these directories.
 
-## License
+    node playlist-matcher.js playlist.m3u8 [del]
+
+Prerequisites
+-------------
+
+-   Node.js - Download and Install
+    [Node.js](http://www.nodejs.org/download/).
+
+License
+-------
+
 (The MIT License)
 
-Permission is hereby granted, free of charge, to any person obtaining
-a copy of this software and associated documentation files (the
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the
 'Software'), to deal in the Software without restriction, including
 without limitation the rights to use, copy, modify, merge, publish,
 distribute, sublicense, and/or sell copies of the Software, and to
 permit persons to whom the Software is furnished to do so, subject to
 the following conditions:
 
-The above copyright notice and this permission notice shall be
-included in all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
-EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
 IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
 CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
--- playlist-matcher.js
+++ playlist-matcher.js
@@ -1,52 +1,91 @@
 var fs = require('fs');
 
-var makeObj = function(array){
-    var hash = {};
-    for (var i = array.length - 1; i >= 0; i--) {
-        hash[array[i]] = true;
-    }
-    return hash;
+var makeObj = function(array) {
+  var hash = {};
+  for (var i = array.length - 1; i >= 0; i--) {
+    hash[array[i]] = true;
+  }
+  return hash;
 };
 
-var objDiff = function (obj1, obj2){
-    var diff = {};
-    for (var prop in obj1){
-        if (!obj2.hasOwnProperty(prop)){
-            diff[prop] = true;
-        }
+var objDiff = function(obj1, obj2) {
+  var diff = {};
+  for (var prop in obj1) {
+    if (!obj2.hasOwnProperty(prop)) {
+      diff[prop] = true;
     }
-    return diff;
+  }
+  return diff;
 };
 
-deleteFolderRecursive = function(path) {
-    var files = [];
-    if( fs.existsSync(path) ) {
-        files = fs.readdirSync(path);
-        files.forEach(function(file,index){
-            var curPath = path + "/" + file;
-            if(fs.statSync(curPath).isDirectory()) { // recurse
-                deleteFolderRecursive(curPath);
-            } else { // delete file
-                fs.unlinkSync(curPath);
-            }
-        });
-        fs.rmdirSync(path);
-    }
+var deleteFolderRecursive = function(path) {
+  var files = [];
+  if (fs.existsSync(path)) {
+    files = fs.readdirSync(path);
+    files.forEach(function(file, index) {
+      var curPath = path + "/" + file;
+      if (fs.statSync(curPath)
+        .isDirectory()) { // recurse
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+    fs.rmdirSync(path);
+  }
 };
 
-var dirObj = makeObj(fs.readdirSync("./"));
+// adapted from http://stackoverflow.com/a/16684530/2116927
+var walk = function(dir) {
+  var results = [];
+  var list = fs.readdirSync(dir);
+  list.forEach(function(file) {
+    file = dir + '/' + file;
+    var stat = fs.statSync(file);
+    if (stat && stat.isDirectory()) results = results.concat(walk(file));
+    else if (file.indexOf(".mp3") != -1 || file.indexOf(".wav") != -1 ||
+      file.indexOf(".flac") != -1 || file.indexOf(".aac") != -1) {
+      // only add dirs with audio files
+      if (file.lastIndexOf("/") > 1) {
+        results.push(file.substring(0, file.lastIndexOf("/")));
+      } else {
+        results.push(file);
+      }
+    }
+  });
+  return results;
+};
+
+var dirObj = makeObj(walk(".")
+  .map(function(file) {
+    // match the path format on the playlist
+    return file.replace('./', '')
+      .replace(/\//g, '\\');
+  }));
 var filesObj = makeObj(
-                    fs.readFileSync("./"+process.argv[2], 'utf8')
-                    .toString().split("\r\n")
-                    .filter(function(line){ return line.charAt(0) != '#';})
-                    .map(function(line){
-                        if (line.indexOf("\\") != -1){
-                            return line.substring(0, line.indexOf("\\"));
-                        }else{
-                            return line;
-                        }
-                    })
-                );
+  fs.readFileSync("./" + process.argv[2], 'utf8')
+  .toString()
+  .split("\r\n")
+  .filter(function(line) {
+    return line.charAt(0) != '#';
+  })
+  .map(function(line) {
+    if (line.indexOf("\\") != -1) {
+      var lines = [];
+      var index = line.indexOf("\\");
+      while (index != -1) {
+        lines.push(line.substring(0, line.indexOf("\\", index)));
+        index = line.indexOf("\\", index + 1);
+      }
+      return lines;
+    } else {
+      return line;
+    }
+  })
+  .reduce(function(previous, current) {
+    return previous.concat(current);
+  }, [])
+);
 var missing = objDiff(filesObj, dirObj);
 var unused = objDiff(dirObj, filesObj);
 
@@ -58,14 +97,13 @@ delete unused["unused.json"];
 fs.writeFileSync('missing.json', JSON.stringify(missing, null, 2));
 fs.writeFileSync('unused.json', JSON.stringify(unused, null, 2));
 
-if (process.argv[3] == "del"){
-    for (var path in unused){
-        if (fs.statSync(path).isDirectory()){
-            deleteFolderRecursive(path);
-        }else{
-            fs.unlinkSync(path);
-        }
+if (process.argv[3] == "del") {
+  for (var path in unused) {
+    if (fs.statSync(path)
+      .isDirectory()) {
+      deleteFolderRecursive(path);
+    } else {
+      fs.unlinkSync(path);
     }
+  }
 }
-
-
